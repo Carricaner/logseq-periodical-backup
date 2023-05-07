c3f567cbf8e191e098f106c63df05af57474a947
scheduler <occa8520@gmail.com>
2023-05-07 17:59:01 +0000

    Scheduled build: 23-05-07_17:59:01

--- schedule.sh
+++ schedule.sh
@@ -7,9 +7,20 @@
 GITHUB_REMOTE_URL="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO_NAME}.git"
 FORMATTED_DATE=$(date +%y-%m-%d_%H:%M:%S)
 
+# Variables checking
+echo $CONTAINER_WORKDIR >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+echo $GITHUB_REMOTE_URL >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+echo $FORMATTED_DATE >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+echo $GIT_USER_NAME >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+echo $GIT_USER_EMAIL >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+echo $GIT_REMOTE_ALIAS >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+echo $GITHUB_USERNAME >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+
+cd ${CONTAINER_WORKDIR}
+
 # git operations
-git config --local user.name "${GIT_USER_NAME}"
-git config --local user.email "${GIT_USER_EMAIL}"
+git config --local user.name "$GIT_USER_NAME"
+git config --local user.email "$GIT_USER_EMAIL"
 
 if [ -z `git remote -v | grep ${GIT_REMOTE_ALIAS}` ]
 then
@@ -20,9 +31,9 @@ git add .
 git reset -- .env
 git commit -m "Scheduled build: ${FORMATTED_DATE}"
 
-if [ -z `git push ${GIT_REMOTE_ALIAS} test-git` ]
-then 
-    echo "Successfully pushed to remote" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-else
-    echo "Error pushing to remote" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-fi
+# if [ -z `git push ${GIT_REMOTE_ALIAS} test-git` ]
+# then 
+#     echo "Successfully pushed to remote" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+# else
+#     echo "Error pushing to remote" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+# fi
