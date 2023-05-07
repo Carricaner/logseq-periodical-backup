efda0a54fdfd64c015d41264ed9e88fcc6932035
scheduler <occa8520@gmail.com>
2023-05-07 10:40:22 +0000

    test

--- schedule.sh
+++ schedule.sh
@@ -3,4 +3,18 @@
 #Source the env file
 . "/usr/src/app/.env"
 
-echo "Var: ${CONTAINER_WORKDIR}123000" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+# Variables
+GITHUB_REMOTE_URL="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${logseq-periodical-backup}.git"
+FORMATTED_DATE=$(date +%y-%m-%d_%H:%M:%S)
+
+# git operations
+git config --local user.name "${GIT_USER_NAME}"
+git config --local user.email "${GIT_USER_EMAIL}"
+if [ -z `git remote -v | grep ${GIT_REMOTE_ALIAS}` ]
+then
+    git remote add ${GIT_REMOTE_ALIAS} ${GITHUB_REMOTE_URL}
+fi
+# git add .
+
+echo "FORMATTED_DATE: ${FORMATTED_DATE}" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+# echo "${GITHUB_REMOTE_URL}" >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
