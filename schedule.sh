6a330c8d0592df27cdce1cf8fd220793ed55368d
scheduler <occa8520@gmail.com>
2023-05-08 17:02:48 +0800

    test successfully in the container

--- schedule.sh
+++ schedule.sh
@@ -5,29 +5,22 @@
 
 # Variables
 GITHUB_REMOTE_URL="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO_NAME}.git"
-FORMATTED_DATE=$(date +%y-%m-%d_%H:%M:%S)
+FORMATTED_DATE=$(date +%y-%m-%dT%H:%M:%S)
 
-# Variables checking
-echo $CONTAINER_WORKDIR >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-echo $GITHUB_REMOTE_URL >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-echo $FORMATTED_DATE >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-echo $GIT_USER_NAME >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-echo $GIT_USER_EMAIL >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-echo $GIT_REMOTE_ALIAS >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
-echo $GITHUB_USERNAME >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
 
-cd ${CONTAINER_WORKDIR}
-
-# git operations
+# Periodically push to github
+cd ${CONTAINER_LOGSEQ_DIR}
 git config --local user.name "$GIT_USER_NAME"
 git config --local user.email "$GIT_USER_EMAIL"
-
 if [ -z `git remote -v | grep ${GIT_REMOTE_ALIAS}` ]
 then
-    git remote add ${GIT_REMOTE_ALIAS} ${GITHUB_REMOTE_URL}
+    git remote add $GIT_REMOTE_ALIAS $GITHUB_REMOTE_URL
 fi
-
 git add .
-git reset -- .env
-git commit -m "Scheduled build: ${FORMATTED_DATE}"
-git push ${GIT_REMOTE_ALIAS} test-git
+git commit -m "Logseq scheduler's update on the time of ${FORMATTED_DATE}"
+git push $GIT_REMOTE_ALIAS $GIT_BRANCH
+
+# Log
+# echo $FORMATTED_DATE >> "${CONTAINER_WORKDIR}/log.txt" 2>&1
+
+echo $FORMATTED_DATE
